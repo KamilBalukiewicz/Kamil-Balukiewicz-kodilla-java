7ed060da6e63b818b960a8b040be2208d9c48625
KamilBalukiewicz <kamilbalukiewicz@gmail.com>
2018-05-20 21:34:44 +0200

    Moduł 7.1

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -0,0 +1,37 @@
+package com.kodilla.stream;
+
+import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.lambda.*;
+import com.kodilla.stream.reference.FunctionalCalculator;
+
+import static jdk.nashorn.internal.objects.NativeString.toUpperCase;
+
+public class StreamMain {
+    public static void main(String[] args) {
+        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
+
+        System.out.println("Calculating expressions with lambdas");
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
+
+        System.out.println("Calculating expressions with method references");
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
+
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
+
+        String poem = poemBeautifier.beautify("sample", str -> toUpperCase(str));
+        String poem1 = poemBeautifier.beautify("sample", str -> "ABC" + str + "ABC");
+        String poem2 = poemBeautifier.beautify("another sample", str -> str.replace('e', 'E'));
+        String poem3 = poemBeautifier.beautify("yet another sample", str -> "_____" + str + "______");
+        System.out.println(poem);
+        System.out.println(poem1);
+        System.out.println(poem2);
+        System.out.println(poem3);
+
+    }
+}
