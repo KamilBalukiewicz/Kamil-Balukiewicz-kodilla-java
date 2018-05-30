bef9fe41d47472b902b014be2a8f0ed8c70d12ef
KamilBalukiewicz <kamilbalukiewicz@gmail.com>
2018-05-30 21:35:28 +0200

    Moduł 8.4

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightFinder.java
@@ -0,0 +1,81 @@
+package com.kodilla.exception.test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FlightFinder {
+
+    public Map<String, Boolean> findFlight(Flight flight) throws RouteNotFoundException {
+        HashMap<String, Boolean> flights = new HashMap<>();
+        flights.put("Warsaw", true);
+        flights.put("New York", true);
+        flights.put("Los Angeles", true);
+        flights.put("Berlin", false);
+
+        if (!flights.containsKey(flight.getArrivalAirport()) || !flights.containsKey(flight.getDepartureAirport())) {
+            throw new RouteNotFoundException("Flight not found");
+        }
+        if (flights.containsKey(flight.getArrivalAirport()) && flights.containsKey(flight.getDepartureAirport())) {
+            boolean value = flights.get(flight.getArrivalAirport());
+            if(value == true) {
+                System.out.println("Flight from " + flight.getDepartureAirport() +
+                        " to " + flight.getArrivalAirport() + " is possible");
+            } else {
+                System.out.println("Flight from " + flight.getDepartureAirport() +
+                        " to " + flight.getArrivalAirport() + " is impossible");
+            }
+
+        }
+
+        return flights;
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+        /*for(Map.Entry<String, Boolean> entry : flights.entrySet()) {
+            String key = entry.getKey();
+            Boolean value = entry.getValue();
+
+            if(key.equals(flight.getArrivalAirport())  &&
+            key.equals(flight.getDepartureAirport()) &&
+                    value == true) {
+                System.out.println("Flight from " + flight.getDepartureAirport() +
+                        " to " + flight.getArrivalAirport() + " is possible");
+            }
+            if(key.equals(flight.getArrivalAirport()) &&
+                    key.equals(flight.getDepartureAirport()) &&
+                    value == false) {
+                System.out.println("Flight from " + flight.getDepartureAirport() +
+                        " to " + flight.getArrivalAirport() + " is impossible");
+            }
+            if(!key.equals(flight.getDepartureAirport()) || !key.equals(flight.getArrivalAirport())) {
+                throw new RouteNotFoundException("Flight not found");
+            }
+        }
+        if (flights.containsKey(flight.getDepartureAirport()) &&
+                flights.containsKey(flight.getArrivalAirport())
+                && flights.containsValue(true)) {
+            System.out.println("Flight from " + flight.getDepartureAirport() +
+                    " to " + flight.getArrivalAirport() + " is possible");
+        }
+        if (flights.containsKey(flight.getDepartureAirport()) &&
+                flights.containsKey(flight.getArrivalAirport())
+                && flights.containsValue(false)) {
+            System.out.println("Flight from " + flight.getDepartureAirport() +
+                    " to " + flight.getArrivalAirport() + " is impossible");
+        }
+
+
+        //throw new RouteNotFoundException("Flight not found");
+        return flights;
+    }
+}*/
---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightFinderRunner.java
@@ -0,0 +1,22 @@
+package com.kodilla.exception.test;
+
+public class FlightFinderRunner {
+    public static void main(String[] args) {
+        Flight flight = new Flight("New York", "Berlin");
+        Flight flight1 = new Flight("Los Angeles", "Warsaw");
+        Flight flight2 = new Flight("Warsaw", "London");
+        Flight flight3 = new Flight("Paris", "Berlin");
+        Flight flight4 = new Flight("Berlin", "Warsaw");
+
+        FlightFinder flightFinder = new FlightFinder();
+
+        try {
+            flightFinder.findFlight(flight);
+        } catch (RouteNotFoundException e) {
+            System.out.println("Sorry. You can't go there");
+        } finally {
+            System.out.println("See where else you could go to");
+        }
+        System.out.println("Choose your journey");
+    }
+}
