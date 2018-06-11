480811a4bf226e586218f62e46b68c408a317671
KamilBalukiewicz <kamilbalukiewicz@gmail.com>
2018-06-11 23:39:14 +0200

    Moduł 10.2 v2

--- kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
@@ -10,49 +10,16 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CalculatorTestSuite {
+
     @Test
-    public void testAdd() {
-        //given
+    public void testCalculations() {
         ApplicationContext context =
                 new AnnotationConfigApplicationContext("com.kodilla.spring");
-        Calculator calculator = context.getBean(Calculator.class);
+        Calculator calculator = (Calculator)context.getBean("calculator");
         //when
         calculator.add(5.5, 4.8);
-        //then
-        //do nothing
-    }
-
-    @Test
-    public void testSub() {
-        //given
-        ApplicationContext context =
-                new AnnotationConfigApplicationContext("com.kodilla.spring");
-        Calculator calculator = context.getBean(Calculator.class);
-        //when
         calculator.sub(15.15, 14.85);
-        //then
-        //do nothing
-    }
-
-    @Test
-    public void testMul() {
-        //given
-        ApplicationContext context =
-                new AnnotationConfigApplicationContext("com.kodilla.spring");
-        Calculator calculator = context.getBean(Calculator.class);
-        //when
         calculator.mul(2.5, 2.5);
-        //then
-        //do nothing
-    }
-
-    @Test
-    public void testDiv() {
-        //given
-        ApplicationContext context =
-                new AnnotationConfigApplicationContext("com.kodilla.spring");
-        Calculator calculator = context.getBean(Calculator.class);
-        //when
         calculator.div(8.5, 0.25);
         //then
         //do nothing
