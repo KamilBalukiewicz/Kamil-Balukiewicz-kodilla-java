917eb490f1cc3ddd3a66bc3113bc2285d786c54e
KamilBalukiewicz <kamilbalukiewicz@gmail.com>
2018-06-13 21:30:24 +0200

    Moduł 10.3

---
+++ kodilla-spring/src/test/java/com/kodilla/spring/portfolio/BoardTestSuite.java
@@ -0,0 +1,23 @@
+package com.kodilla.spring.portfolio;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class BoardTestSuite {
+    @Test
+    public void testTaskAdd() {
+        //given
+        ApplicationContext context = new AnnotationConfigApplicationContext(BoardConfig.class);
+        Board board = context.getBean(Board.class);
+        //when
+        board.toDoList.getTasks().add("task1");
+        board.inProgressList.getTasks().add("task2");
+        board.doneList.getTasks().add("task3");
+        //then
+        System.out.println(board.toDoList.getTasks().get(0) + "\n" +
+                board.inProgressList.getTasks().get(0) + "\n" +
+                board.doneList.getTasks().get(0));
+
+    }
+}
